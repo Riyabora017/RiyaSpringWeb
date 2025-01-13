8a56822e0697ae5c947b11eb2afe96a3b7da399b
Riya bora <borariya982@gmail.com>
2025-01-13 15:52:33 +0530

    Update

--- src/main/java/com/springweb/controller/welcomeController.java
+++ src/main/java/com/springweb/controller/welcomeController.java
@@ -1,10 +1,13 @@
 package com.springweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class welcomeController {
+public class WelcomeController {
 
 	@RequestMapping("/")
 	public String greet() {
@@ -13,9 +16,16 @@ public class welcomeController {
 	}
 
 	@RequestMapping("/hello")
-	public String message() {
-		System.out.println("WelcomeController.message()");
+	public String message(@RequestParam(name = "user", defaultValue = "Default value...") String name) {
+		System.out.println("WelcomeController.message()   " + name);
 		return "index";
+	}
+
+	@RequestMapping("/get/{id}")
+	@ResponseBody
+	public String pathVar(@PathVariable(name = "id") int id) {
+		System.out.println("WelcomeController  pathVar()   " + id);
+		return "This is my response to client that your id is: "+id;
 	}
 
 }
