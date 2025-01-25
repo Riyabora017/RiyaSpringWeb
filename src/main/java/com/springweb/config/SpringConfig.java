8ca2da5b5d3b2f3c2acba2f83ae2ad6f7c3f14cb
Riya bora <borariya982@gmail.com>
2025-01-25 17:02:30 +0530

    update

--- src/main/java/com/springweb/config/SpringConfig.java
+++ src/main/java/com/springweb/config/SpringConfig.java
@@ -2,23 +2,25 @@ package com.springweb.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.springweb")
 public class SpringConfig implements WebMvcConfigurer {
 
 	@Bean
 	public ViewResolver viewResolver() {
+		System.out.println("SpringConfig.viewResolver()");
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/JSP/");
 		viewResolver.setSuffix(".jsp");
-
 		return viewResolver;
 	}
 
--- src/main/webapp/WEB-INF/JSP/index.jsp
+++ src/main/webapp/WEB-INF/JSP/index.jsp
@@ -1,12 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
 </head>
-<body>
-<h1>HELLO THIS IS MY Second RESPONSE TO CLIENT</h1>
+<body bgcolor="pink">
+	<h1>This is my Home page</h1>
 </body>
-</html>>
+</html>
